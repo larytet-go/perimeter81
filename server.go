1e88c8f1ca0495c8a23478169266b63d05759724
arkady <larytet@yahoo.com>
2021-02-19 07:40:02 +0200

    code

--- server.go
+++ server.go
@@ -6,8 +6,38 @@ import (
     "net/http"
 )
 
+
+type ControlPanel struct {
+	port int
+}
+
+func (cp *ControlPanel) totals(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Totals")
+}
+
+// shortcut: for 1M sensors I need a better UI
+func (cp *ControlPanel) sensors(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "sensors")
+}
+
+func (cp *ControlPanel) start() {
+	http.HandleFunc("/totals", cp.totals)	
+	http.HandleFunc("/sensors", cp.totals)	
+	
+	http.ListenAndServe(cp.port, nil)
+}
+
+
+
+func dataPath() {
+
+}
+
+
+
 func main() {
-	// start data plane loop 
-	go dataPlane()
+	// start data path loop 
+	go dataPath()
 	// start control loop
+	go controlPanel()
 }
